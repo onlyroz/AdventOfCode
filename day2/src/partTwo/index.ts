6806b06f140a9ed3560b3693381d97690b86bd56
rosiechandler <rosie.chandler@67bricks.com>
2024-12-01 00:23:52 +0000

    day 2 initial

---
+++ day2/src/partTwo/index.ts
@@ -0,0 +1,7 @@
+import { shout } from "../utils";
+
+export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+
+export const partTwoShout = async (input = [] as string[]) => {
+  shout("Hello from Part Two!");
+};
