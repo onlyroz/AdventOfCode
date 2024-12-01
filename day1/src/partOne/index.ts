77232eeb0a14a1d922532137f3bf4a203b6b8284
rosiechandler <rosie.chandler@67bricks.com>
2024-12-01 10:44:14 +0000

    AoC day 1

--- day1/src/partOne/index.ts
+++ day1/src/partOne/index.ts
@@ -2,4 +2,22 @@ import { shout } from "../utils";
 
 export const partOneShout = async (input = [] as string[]) => {
   shout("Hello from Part One");
+
+  const list1: number[] = [];
+  const list2: number[] = [];
+
+  input.forEach(line => {
+    const [a, b] = line.split("   ");
+    list1.push(Number(a));
+    list2.push(Number(b));
+  });
+
+  list1.sort((a, b) => a - b);
+  list2.sort((a, b) => a - b);
+
+  const sum = list1.reduce((acc, val, i) => 
+    acc + Math.abs(list2[i] - val), 0
+  );
+
+  shout(`The answer is ${sum}`);
 };
--- day1/src/partTwo/index.ts
+++ day1/src/partTwo/index.ts
@@ -4,4 +4,24 @@ export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
 
 export const partTwoShout = async (input = [] as string[]) => {
   shout("Hello from Part Two!");
+
+  const list1: number[] = [];
+  const list2: number[] = [];
+
+  input.forEach(line => {
+    const [a, b] = line.split("   ");
+    list1.push(Number(a));
+    list2.push(Number(b));
+  });
+
+  const list2Freq = list2.reduce((freq, num) => {
+    freq[num] = (freq[num] || 0) + 1;
+    return freq;
+  }, {} as Record<number, number>);
+
+  const sumOfSimilarities = list1.reduce((acc, num) => 
+    acc + (num * (list2Freq[num] || 0)), 0
+  ); 
+
+  shout(`The answer is ${sumOfSimilarities}`);
 };
