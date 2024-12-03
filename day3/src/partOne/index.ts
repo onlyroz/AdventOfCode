21a3e22777624c964d633849562a93ca851117d9
rosiechandler <rosie.chandler@67bricks.com>
2024-12-03 23:50:35 +0000

    day 3

--- day3/src/partOne/index.ts
+++ day3/src/partOne/index.ts
@@ -2,4 +2,17 @@ import { shout } from "../utils";
 
 export const partOneShout = async (input = [] as string[]) => {
   shout("Hello from Part One");
+
+  const regex = /mul\(([\d, ]+)\)/g;
+
+  let sum = 0n;
+  for (let line of input) {
+    const matches = line.match(regex);
+    matches?.forEach((match) => {
+      const [a, b] = match.replace('mul(', '').replace(')', '').split(',');
+      sum += (BigInt(a) * BigInt(b));
+    });
+  }
+
+  shout(`sum: ${sum.toString()}`); 
 };
--- day3/src/partTwo/index.ts
+++ day3/src/partTwo/index.ts
@@ -2,4 +2,24 @@ import { shout } from "../utils";
 
 export const partTwoShout = async (input = [] as string[]) => {
   shout("Hello from Part Two!");
+
+  const regex = /(?:mul\((\d+),(\d+)\)|do\(\)|don't\(\))/g;
+
+  let sum = 0n;
+  let include = true;
+  for (let line of input) {
+    const matches = line.match(regex);
+    matches?.forEach((match) => {
+      if (match === "do()") {
+        include = true;
+      } else if (match === "don't()") {
+        include = false;
+      } else if (include && match.startsWith('mul')) {
+        const [a, b] = match.replace('mul(', '').replace(')', '').split(',');
+        sum += (BigInt(a) * BigInt(b));
+      }
+    });
+  } 
+
+  shout(`sum: ${sum.toString()}`); 
 };
