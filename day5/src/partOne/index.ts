e44c69401cba8c3f2aaf2a7e9f829f98771ecdc4
rosiechandler <rosie.chandler@67bricks.com>
2024-12-05 08:18:51 +0000

    day 5 part 1

--- day5/src/partOne/index.ts
+++ day5/src/partOne/index.ts
@@ -2,4 +2,40 @@ import { shout } from "../utils";
 
 export const partOneShout = async (input = [] as string[]) => {
   shout("Hello from Part One");
+
+  const rules: { left: number; right: number; }[] = [];
+  const updates: number[][] = [];
+  let rulesRead = false;
+
+  input.forEach(line => {
+    if (line === "") {
+      rulesRead = true;
+      return;
+    }
+    if (!rulesRead) {
+      const [left, right] = line.split('|');
+      rules.push({
+        left: Number(left),
+        right: Number(right),
+      });
+    }
+    if (rulesRead) {
+      updates.push(line.split(',').map(Number));
+    }
+  });
+
+  const sum = updates.reduce((acc, update) => {
+    const isValid = update.every((num, idx, self) => {
+      const tail = self.slice(idx + 1);
+
+      const invalid = tail.some(tailNum => 
+        rules.some(rule => num === rule.right && tailNum === rule.left)
+      );
+      return !invalid;
+    });
+
+    return acc + (isValid ? update[Math.round((update.length-1) / 2)] : 0);
+  }, 0);
+
+  shout(`Sum: ${sum}`);
 };
