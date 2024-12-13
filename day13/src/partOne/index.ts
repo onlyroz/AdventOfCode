1b3b4997747feaf9e0386ddb036c58356b4c8f0f
rosiechandler <rosie.chandler@67bricks.com>
2024-12-13 08:37:09 +0000

    day 13

--- day13/src/partOne/index.ts
+++ day13/src/partOne/index.ts
@@ -2,4 +2,46 @@ import { shout } from "../utils";
 
 export const partOneShout = async (input = [] as string[]) => {
   shout("Hello from Part One");
+
+  let a1 = 0;
+  let a2 = 0;
+  let b1 = 0;
+  let b2 = 0;
+  let c1 = 0;
+  let c2 = 0;
+  let count = 0;
+  input.push('');
+
+  input.forEach(line => {
+    if (line === '') {
+      const na = ((c1 * b2) - (c2 * b1)) / ((a1 * b2) - (b1 * a2));
+      const nb = ((c2 * a1) - (c1 * a2)) / ((a1 * b2) - (b1 * a2));
+
+      if (Number.isInteger(na) && Number.isInteger(nb)) {
+        count += (3 * na) + nb;
+      }
+    }; 
+
+    const modifiedLine = line.replaceAll(' ', '').replace('X+', '').replace('Y+', '').replace('X=', '').replace('Y=', '');
+
+    if (modifiedLine.includes('ButtonA')) {
+      const [x, y] = modifiedLine.replace('ButtonA:', '').split(',').map(x => parseInt(x));
+      a1 = x;
+      a2 = y;
+    }
+
+    if (modifiedLine.includes('ButtonB')) {
+      const [x, y] = modifiedLine.replace('ButtonB:', '').split(',').map(x => parseInt(x));
+      b1 = x;
+      b2 = y;
+    }
+
+    if (modifiedLine.includes('Prize')) {
+      const [x, y] = modifiedLine.replace('Prize:', '').split(',').map(x => parseInt(x));
+      c1 = x;
+      c2 = y;
+    }
+  });
+
+  shout(`Part One: ${count}`);
 };
--- day13/src/partTwo/index.ts
+++ day13/src/partTwo/index.ts
@@ -2,4 +2,46 @@ import { shout } from "../utils";
 
 export const partTwoShout = async (input = [] as string[]) => {
   shout("Hello from Part Two!");
+
+  let a1 = 0;
+  let a2 = 0;
+  let b1 = 0;
+  let b2 = 0;
+  let c1 = 0;
+  let c2 = 0;
+  let count = 0;
+  input.push('');
+
+  input.forEach(line => {
+    if (line === '') {
+      const na = ((c1 * b2) - (c2 * b1)) / ((a1 * b2) - (b1 * a2));
+      const nb = ((c2 * a1) - (c1 * a2)) / ((a1 * b2) - (b1 * a2));
+
+      if (Number.isInteger(na) && Number.isInteger(nb)) {
+        count += (3 * na) + nb;
+      }
+    }; 
+
+    const modifiedLine = line.replaceAll(' ', '').replace('X+', '').replace('Y+', '').replace('X=', '').replace('Y=', '');
+
+    if (modifiedLine.includes('ButtonA')) {
+      const [x, y] = modifiedLine.replace('ButtonA:', '').split(',').map(x => parseInt(x));
+      a1 = x;
+      a2 = y;
+    }
+
+    if (modifiedLine.includes('ButtonB')) {
+      const [x, y] = modifiedLine.replace('ButtonB:', '').split(',').map(x => parseInt(x));
+      b1 = x;
+      b2 = y;
+    }
+
+    if (modifiedLine.includes('Prize')) {
+      const [x, y] = modifiedLine.replace('Prize:', '').split(',').map(x => parseInt(x));
+      c1 = x + 10000000000000;
+      c2 = y + 10000000000000;
+    }
+  });
+
+  shout(`Part One: ${count}`);
 };
