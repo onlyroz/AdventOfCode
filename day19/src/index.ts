a221bfdfb3452bfde3d0e456bcaaf7382753ccc4
rosiechandler <rosie.chandler@67bricks.com>
2024-12-15 09:15:23 +0000

    Placeholders for days 16-20

---
+++ day19/src/index.ts
@@ -0,0 +1,13 @@
+import { partOneShout } from "./partOne";
+import { partTwoShout } from "./partTwo";
+import { readFile, readTestFile } from "./utils";
+
+async function main() {
+  partOneShout(await readTestFile());
+  partOneShout(await readFile());
+
+  partTwoShout(await readTestFile());
+  partTwoShout(await readFile());
+}
+
+main();
