d00b99bd8794db70428e162d6e42dcc286827e2e
rosiechandler <rosie.chandler@67bricks.com>
2024-12-24 21:18:49 +0000

    day 24

--- day24/src/partOne/index.ts
+++ day24/src/partOne/index.ts
@@ -2,4 +2,66 @@ import { shout } from "../utils";
 
 export const partOneShout = async (input = [] as string[]) => {
   shout("Hello from Part One");
+
+  type Operation = {
+    output: string;
+    operator: string;
+  }
+
+  const wires = new Map<string, number | undefined>();
+  const operations = new Map<string, Operation>();
+
+  for (const line of input) {
+    if (line.includes(": ")) {
+      const [wire, value] = line.split(": ");
+      wires.set(wire, Number(value));
+    } else if (line !== "") {
+      const [input, output] = line.split(" -> ");
+      const [left, operator, right] = input.split(" ");
+      operations.set(`${left},${right},${output}`, { output, operator });
+
+      if (!wires.has(left)) wires.set(left, undefined);
+      if (!wires.has(right)) wires.set(right, undefined);
+      if (!wires.has(output)) wires.set(output, undefined);
+    }
+  }
+  
+  const hasAllZValues = () => Array.from(wires.entries())
+    .filter(([key, _]) => key.startsWith("z"))
+    .every(([_, value]) => typeof value !== "undefined");
+
+  while (!hasAllZValues()) {
+    for (const [key, operation] of operations.entries()) {
+      const [left, right] = key.split(",");
+      const leftWire = wires.get(left);
+      const rightWire = wires.get(right);
+
+      if (typeof leftWire === "undefined" || typeof rightWire === "undefined") 
+        continue;
+
+      if (operation.operator === "AND") {
+        wires.set(operation.output, leftWire && rightWire);
+      } else if (operation.operator === "OR") {
+        wires.set(operation.output, leftWire || rightWire);
+      } else if (operation.operator === "XOR") {
+        wires.set(operation.output, leftWire ^ rightWire);
+      }
+
+      operations.delete(key);
+    }
+  }
+
+  const zWires = Array.from(wires.entries())
+    .filter(([key, _]) => key.startsWith("z"))
+    .sort((a, b) => {
+      const numA = parseInt(a[0].substring(1));
+      const numB = parseInt(b[0].substring(1));
+      return numB - numA;
+    })
+    .map(([_, value]) => value)
+    .join("");
+
+  const decimal = parseInt(zWires, 2);
+  shout(`Binary: ${zWires}`);
+  shout(`Decimal: ${decimal}`);
 };
--- day24/src/partTwo/index.ts
+++ day24/src/partTwo/index.ts
@@ -1,5 +1,54 @@
 import { shout } from "../utils";
 
 export const partTwoShout = async (input = [] as string[]) => {
-  shout("Hello from Part Two!");
+  const instructions = input.filter(line => line.includes('->')).map(line => {
+    const tokens = line.split(' ');
+    return { a: tokens[0], b: tokens[2], c: tokens[4], operation: tokens[1] };
+   });
+
+  const BIT_LENGTH = 45;
+  const incorrect: string[] = [];
+
+  for (let i = 0; i < BIT_LENGTH; i++) {
+      const id = i.toString().padStart(2, '0');
+
+      const xor1 = instructions.find(instruction => 
+        ((instruction.a === `x${id}` && instruction.b === `y${id}`) || 
+          (instruction.a === `y${id}` && instruction.b === `x${id}`)) && 
+        instruction.operation === 'XOR');
+
+      const and1 = instructions.find(instruction => 
+        ((instruction.a === `x${id}` && instruction.b === `y${id}`) || 
+          (instruction.a === `y${id}` && instruction.b === `x${id}`)) && 
+          instruction.operation === 'AND');
+
+      const z = instructions.find(instruction => instruction.c === `z${id}`);
+
+      if (xor1 === undefined || and1 === undefined || z === undefined) continue;
+
+      // each z must be connected to an XOR
+      if (z.operation !== 'XOR') incorrect.push(z.c);
+      
+      // each AND must go to an OR (besides the first case as it starts the carry flag)
+      const or = instructions.find(instruction => 
+        instruction.a === and1.c || instruction.b === and1.c);
+
+      if (or !== undefined && or.operation !== 'OR' && i > 0) incorrect.push(and1.c);
+
+      // the first XOR must to go to XOR or AND
+      const after = instructions.find(instruction => 
+        instruction.a === xor1.c || instruction.b === xor1.c);
+
+      if (after !== undefined && after.operation === 'OR') incorrect.push(xor1.c);
+  }
+
+  // each XOR must be connected to an x, y, or z
+  incorrect.push(...instructions.filter(instruction => 
+    !instruction.a[0].match(/[xy]/g) && 
+    !instruction.b[0].match(/[xy]/g) && 
+    !instruction.c[0].match(/[z]/g) && 
+    instruction.operation === 'XOR'
+  ).map(instruction => instruction.c))
+
+  shout(incorrect.sort().join(',')); 
 };
